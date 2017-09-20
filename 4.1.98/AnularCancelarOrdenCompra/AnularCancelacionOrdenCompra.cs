02cfd1e19f3d8d30ad16a1d850275afd2a85e1ab
Juan De Dios <jbaudazio@gmail.com>
2017-09-19 22:39:32 -0500

    Test Case
    Casos de Pruebas

---
+++ 4.1.98/AnularCancelarOrdenCompra/AnularCancelacionOrdenCompra.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+using System.Windows.Forms;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UITest.Extension;
+using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+
+
+namespace AnularCancelarOrdenCompra
+{
+    /// <summary>
+    /// Descripción resumida de CodedUITest1
+    /// </summary>
+    [CodedUITest]
+    public class AnularCancelacionOrdenCompra
+    {
+        public AnularCancelacionOrdenCompra()
+        {
+        }
+
+        [TestMethod]
+        public void CUITAnularCancelacionOrdenCompra()
+        {
+            // Para generar código para esta prueba, seleccione "Generar código para prueba de IU codificada" en el menú contextual y seleccione uno de los elementos de menú.
+            this.UIMap.SeleccionarOrdendecomprayAnularlaCancelaciondecomprobante();
+            this.UIMap.Aceptar();
+
+        }
+
+        #region Atributos de prueba adicionales
+
+        // Puede usar los siguientes atributos adicionales conforme escribe las pruebas:
+
+        ////Use TestInitialize para ejecutar el código antes de ejecutar cada prueba 
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{        
+        //    // Para generar código para esta prueba, seleccione "Generar código para prueba de IU codificada" en el menú contextual y seleccione uno de los elementos de menú.
+        //}
+
+        ////Use TestCleanup para ejecutar el código después de ejecutar cada prueba
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{        
+        //    // Para generar código para esta prueba, seleccione "Generar código para prueba de IU codificada" en el menú contextual y seleccione uno de los elementos de menú.
+        //}
+
+        #endregion
+
+        /// <summary>
+        ///Obtiene o establece el contexto de las pruebas que proporciona
+        ///información y funcionalidad para la serie de pruebas actual.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+        private TestContext testContextInstance;
+
+        public UIMap UIMap
+        {
+            get
+            {
+                if ((this.map == null))
+                {
+                    this.map = new UIMap();
+                }
+
+                return this.map;
+            }
+        }
+
+        private UIMap map;
+    }
+}
